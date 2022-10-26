bfa082df0a1729e403b062c796bd0805652ffc96
Shedrack Sunday <Hannania002@gmail.com>
2022-10-26 15:55:46 +0100

    Convert function class to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+/* eslint-disable */
 import Calculator from './components/Calculator';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+          <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
