3addf4e0fd5783e8bf6bf14fb7380b20700a3034
Shedrack Sunday <Hannania002@gmail.com>
2022-11-02 07:24:38 +0100

    Add refactored app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 /* eslint-disable */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
-import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div>
-      <Calculator />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
