04428aa02ebc545da12ea40eb476cf5a528b64a6
Shedrack Sunday <Hannania002@gmail.com>
2022-11-02 21:04:15 +0100

    Add claculate test file

---
+++ src/components/_tests_/Calculator.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Calculator from '../Calculator';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Test render Calculator component', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+    .create(<Calculator />)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
