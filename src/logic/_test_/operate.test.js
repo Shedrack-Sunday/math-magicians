9b6f5dbca428ba956993a7d18bd1001d0ec52f8f
Shedrack Sunday <Hannania002@gmail.com>
2022-11-02 22:43:44 +0100

    Add operate extra test case

--- src/logic/_test_/operate.test.js
+++ src/logic/_test_/operate.test.js
@@ -7,7 +7,13 @@ describe('Testing operate.js file', () => {
   it('Test multiply 3 * 3 = 9', () => {
     expect(operate('3', '3', 'x')).toBe('9');
   });
-  it('Test multiply 5 / 3 = undefined', () => {
+  it('Test divide 5 / 0 = undefined', () => {
     expect(operate('3', '0', '/')).toBe("Can't divide by 0.");
+  });
+  it('Test minus 6 - 3 = 3', () => {
+    expect(operate('6', '3', '-')).toBe('3');
+  });
+  it('Test modulus 5 / 3 = 2', () => {
+    expect(operate('5', '3', '%')).toBe('2');
   });
 });
